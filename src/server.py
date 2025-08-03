b52b23355ff2179c7cb145c45bb3d00f213690a4
Lee Si Woong <silverttthin@naver.com>
2025-08-03 21:55:34 +0900

    chore: 주석 이동

--- src/server.py
+++ src/server.py
@@ -2,6 +2,10 @@ import uvicorn
 import os
 from .utils import load_env_config
 
+
+# poetry run dev로 실행시키는 파일, 즉 서버실행용 파일이다. 현재 환경에 맞는 서버를 실행시킨다. 지금은 배포나 dev나 별차이없음
+
+
 def run_dev_server():
     os.environ['PYTHON_ENV'] = 'development'
     uvicorn.run("src.app:app", reload=True)
@@ -11,7 +15,6 @@ def run_production_server():
     os.environ['PYTHON_ENV'] = 'production'
     uvicorn.run("src.app:app", reload=True)
 
-# poetry run dev로 실행시키게될 파일. 환경에 맞는 서버를 실행시킨다.
 if __name__ == "__main__":
     env = load_env_config()
     run_production_server() if env == "production" else run_dev_server()
